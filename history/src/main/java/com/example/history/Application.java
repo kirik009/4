1ff0ae59b58a029c3d3b2695794c3667a2deeebf
21598 <21598>
2023-12-07 14:20:32 +0200

    2_commit

--- history/src/main/java/com/example/history/Application.java
+++ history/src/main/java/com/example/history/Application.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Start");
